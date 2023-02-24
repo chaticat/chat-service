915419643daa5449da836c2f5b405cc7513c395e
stevi <trozzato@gmail.com>
2023-02-24 18:09:47 +0200

    init

---
+++ src/main/java/com/chaticat/chatmanagementservice/ChatManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chaticat.chatmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chaticat/chatmanagementservice/ChatManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaticat.chatmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
